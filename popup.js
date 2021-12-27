41a2821744f40233965a7371d08fccf011abe956
John Newman <newmanj903@gmail.com>
2021-12-27 21:30:23 +0000

    testing out some stuff 😀" "

--- popup.js
+++ popup.js
@@ -1,20 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const bg = chrome.extension.getBackgroundPage()
-const div = document.createElement('div')
-div.textContent = bg.bears
-document.body.appendChild(div)
+let checkHistory = document.getElementById("checkHistory");
+console.log('running');
 
-  // document.querySelector('button').addEventListener('click', onclick, false)
-  //
-  // function onclick () {
-  //   chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-  //     chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
-  //   })
-  // }
-  //
-  // function setCount (res) {
-  //   const div = document.createElement('div')
-  //   div.textContent = `${res.count} bears`
-  //   document.body.appendChild(div)
-  // }
-}, false)
+checkHistory.addEventListener("click", async () => {
+  checkOutSomeHistory();
+
+});
+
+function checkOutSomeHistory() {
+  var microsecondsPerDay= 1000 * 60 * 60 * 24;
+  console.log('made it here');
+  var oneDayAgo = (new Date).getTime() - microsecondsPerDay;
+  chrome.history.search({
+    'text': '',              // Return every history item....
+    'startTime': oneDayAgo  // that was accessed less than one day ago.
+  },
+  function(historyItems) {
+
+    for (var i = 0; i < historyItems.length; ++i) {
+      var url = historyItems[i].url;
+      chrome.history.getVisits({url: url}, function(visitItems) {
+        console.log(visitItems)
+      })
+    };
+  });
+}
+
