ab6292840f554c4eff8c9a742cb790b24e289d46
John Newman <newmanj903@gmail.com>
2021-12-28 11:21:44 +0000

    add eslint

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,36 +1,28 @@
-// let checkHistory = document.getElementById("checkHistory");
-
-// checkHistory.addEventListener("click", async () => {
-//   checkOutSomeHistory();
-
-// });
-
-// function checkOutSomeHistory2() {
-//   var microsecondsPerDay= 1000 * 60 * 60 * 24;
-//   var oneDayAgo = (new Date).getTime() - microsecondsPerDay;
-//   chrome.history.search({
-//     'text': '',              
-//     'startTime': oneDayAgo  
-//   },
-//   function(historyItems) {
-
-//     for (var i = 0; i < historyItems.length; ++i) {
-//       var url = historyItems[i].url;
-//       chrome.history.getVisits({url: url}, function(visitItems) {
-//         console.log(visitItems)
-//       })
-//     };
-//   });
-// }
-
-import {h,  FunctionComponent } from "preact";
+import { h,  FunctionComponent } from "preact";
 
 const App: FunctionComponent = () => {
-        return (
-            <div id="app-root">
-                <button>Check History</button>
-            </div>
-        )
+  const checkOutSomeHistory = () => {
+    const microsecondsPerDay= 1000 * 60 * 60 * 24;
+    const oneDayAgo = (new Date).getTime() - microsecondsPerDay;
+
+    chrome.history.search({
+      'text': '',              
+      'startTime': oneDayAgo  
+    },
+    function(historyItems) {
+      for (let i = 0; i < historyItems.length; ++i) {
+        const url = historyItems[i].url;
+        chrome.history.getVisits({url: url}, function(visitItems) {
+          console.log(visitItems)
+        })
+      }
+    });
+  }
+      return (
+          <div id="app-root">
+              <button onClick={checkOutSomeHistory}>Check History</button>
+          </div>
+      )
 }
 
 export default App;
